cbaa815f68e265564714aa076365b6995b9e05f4
Raja-Vardhan <84737078+RajaVardhan369@users.noreply.github.com>
2021-08-24 16:00:35 +0530

    Add files via upload

---
+++ Swapping_Two_Variables.py
@@ -0,0 +1,13 @@
+#Python Program To Swap Two Variables
+
+x = 20
+y = 50
+
+#Create A Temporary Variables And Swap The Values
+
+temp = x
+x = y
+y = temp
+
+print('The Value of x After Swapping: {}'.format(x))
+print('The Value of y After Swapping: {}'.format(y))
