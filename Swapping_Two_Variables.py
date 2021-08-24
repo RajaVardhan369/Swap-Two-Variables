bb844008ee6b8d68ef0fd2a93f460470f9a2640e
DhananjineyuluGogula <dhananjineyulugogula@gmail.com>
2021-08-24 16:18:27 +0530

    Simple and Easy Way To Swap

--- Swapping_Two_Variables.py
+++ Swapping_Two_Variables.py
@@ -11,3 +11,15 @@ y = temp
 
 print('The Value of x After Swapping: {}'.format(x))
 print('The Value of y After Swapping: {}'.format(y))
+
+#Also We Have Another Way To Swap Two Variables
+
+#Here Is The Result
+
+x = 15
+y = 20
+
+x,y = y,x
+
+print('After Swapping x is {}'.format(x))
+print('After Swapping x is {}'.format(y))
